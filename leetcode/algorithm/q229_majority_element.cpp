bb1d47f0ed02cdce4007874f1f996c37f702baab
acreast <letnux@gmail.com>
2023-10-05 23:09:50 +0800

    q229

---
+++ leetcode/algorithm/q229_majority_element.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/majority-element-ii/description/?envType=daily-question&envId=2023-10-05
+
+// what is this?
+// https://www.geeksforgeeks.org/boyer-moore-majority-voting-algorithm/
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        // Create a frequency map to store the count of each element
+        unordered_map<int, int> elementCountMap;
+        
+        // Iterate through the input array to count element occurrences
+        for(int i = 0; i < nums.size(); i++) {
+            elementCountMap[nums[i]]++;
+        }
+        
+        vector<int> majorityElements;
+        int threshold = nums.size() / 3;
+        
+        // Iterate through the frequency map to identify majority elements
+        for(auto elementCountPair : elementCountMap) {
+            int element = elementCountPair.first;
+            int count = elementCountPair.second;
+            
+            // Check if the element count is greater than the threshold
+            if(count > threshold) {
+                majorityElements.push_back(element);
+            }
+        }
+        
+        return majorityElements; 
+    }
+};
