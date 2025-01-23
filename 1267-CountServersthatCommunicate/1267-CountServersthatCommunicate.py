0e98a58607faaed7339e986ce0ad9a363401f57d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-01-24 00:03:30 +0800

    Create file

---
+++ 1267-CountServersthatCommunicate/1267-CountServersthatCommunicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        rows = [0] * ROWS
+        cols = [0] * COLS 
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    rows[r] += 1
+                    cols[c] += 1
+        
+        res = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] and max(rows[r], cols[c]) > 1:
+                    res += 1
+
+        return res
