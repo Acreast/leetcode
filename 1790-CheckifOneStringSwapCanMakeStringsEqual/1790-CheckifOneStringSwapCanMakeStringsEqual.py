21398346e0868895741c6df490539d0217cce2e1
Yuan <65935215+Acreast@users.noreply.github.com>
2025-02-05 23:13:41 +0800

    Update file 2/5/2025, 11:13:40 PM

--- 1790-CheckifOneStringSwapCanMakeStringsEqual/1790-CheckifOneStringSwapCanMakeStringsEqual.py
+++ 1790-CheckifOneStringSwapCanMakeStringsEqual/1790-CheckifOneStringSwapCanMakeStringsEqual.py
@@ -1,21 +1,18 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        first_diff_index = None
-        second_diff_index = None
-        diff_count = 0
-        if len(s1) != len(s2):
-            return False
+        
+        if s1 == s2:
+            return True
+        
+        diff_indexes = []
 
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                diff_count += 1
-                if first_diff_index == None:
-                    first_diff_index = i
-                else:
-                    second_diff_index = i
-        
-        if diff_count == 0:
-            return True
-        if diff_count > 2 or diff_count == 1:
+                diff_indexes.append(i)
+        if len(diff_indexes) != 2:
             return False
-        return s1[first_diff_index] == s2[second_diff_index] and s1[second_diff_index] == s2[first_diff_index]
+            
+        if s1[diff_indexes[0]] == s2[diff_indexes[1]] and s2[diff_indexes[0]] == s1[diff_indexes[1]]:
+            return True
+
+        return False
