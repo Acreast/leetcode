eed2e9cfde48abca5ae0d692e00ca7f95e43470d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-02-08 20:48:11 +0800

    Update file 2/8/2025, 8:48:10 PM

--- 2349-DesignaNumberContainerSystem/2349-DesignaNumberContainerSystem.py
+++ 2349-DesignaNumberContainerSystem/2349-DesignaNumberContainerSystem.py
@@ -1,23 +1,31 @@
-import heapq
+from sortedcontainers import SortedSet
 
 class NumberContainers:
+
     def __init__(self):
-        self.index_val = {}
-        self.res = {}
+        self.index_val = {}  # Maps index -> number
+        self.res = {}  # Maps number -> SortedSet of indices
 
     def change(self, index: int, number: int) -> None:
         if index in self.index_val:
-            prevNum = self.index_val[index]
-            if prevNum == number:
+            prev_number = self.index_val[index]
+            if prev_number == number:
                 return
-            self.res[prevNum].discard(index)
+            self.res[prev_number].discard(index)
+            if not self.res[prev_number]:  # Clean up empty sets
+                del self.res[prev_number]
 
         if number not in self.res:
-            self.res[number] = set()
+            self.res[number] = SortedSet()
         self.res[number].add(index)
         self.index_val[index] = number
 
     def find(self, number: int) -> int:
         if number not in self.res or not self.res[number]:
             return -1
-        return min(self.res[number])
+        return self.res[number][0]  # O(1) time to get min value
+
+# Usage
+# obj = NumberContainers()
+# obj.change(index, number)
+# param_2 = obj.find(number)
