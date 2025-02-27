1c11d9f5a4631e8093b0c76d30d18ca129b97f78
Yuan <65935215+Acreast@users.noreply.github.com>
2025-02-27 23:56:03 +0800

    Create file

---
+++ 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        res = 0
+        arr_set = set(arr)
+
+
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+
+                prev, cur = arr[i], arr[j]
+                next = cur + prev
+                length = 2
+                while next in arr_set:
+                    length += 1
+                    prev = cur
+                    cur = next
+                    next = prev + cur
+                    res = max(res, length)
+        return res
+                    
