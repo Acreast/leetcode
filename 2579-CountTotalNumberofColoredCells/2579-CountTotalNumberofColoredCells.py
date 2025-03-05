dd5a1fab974751185974a8ea68f31a463d678ff2
Yuan <65935215+Acreast@users.noreply.github.com>
2025-03-05 21:10:16 +0800

    File updated at 3/5/2025, 9:10:27 PM

--- 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.py
+++ 2579-CountTotalNumberofColoredCells/2579-CountTotalNumberofColoredCells.py
@@ -1,4 +1,6 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        
+        # Gauss formula logic:
+        # 1 + the last number times the total count of n divided by two
+        # Essentially half the number of (1 + last number) pairs
         return int(1+(4*(n - 1))/2 * n)
