39f85f86e8aff217123d683cf8e2d1fd5a1dc448
Yuan <65935215+Acreast@users.noreply.github.com>
2025-03-15 15:29:17 +0800

    Create file

---
+++ 2560-HouseRobberIV/2560-HouseRobberIV.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        def is_capable(capability):
+            i = 0
+            count = 0
+            while i < len(nums):
+                if nums[i] <= capability:
+                    i += 2
+                    count += 1
+                else:
+                    i += 1
+                if count == k:
+                    break
+            return count == k
+
+        l = min(nums)
+        r = max(nums)
+        res = 0
+
+        while l <= r:
+            mid = (l + r) // 2
+            if is_capable(mid):
+                res = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return res
