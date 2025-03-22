50b474ca9c689efe0ef7e741fc17d922c1b9029d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-03-22 13:29:08 +0800

    Create file

---
+++ 2685-CounttheNumberofCompleteComponents/2685-CounttheNumberofCompleteComponents.py
@@ -0,0 +1,35 @@
+// Last updated: 3/22/2025, 1:29:12 PM
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        def dfs(v, res):
+            if v in visited:
+                return 
+            visited.add(v)
+            res.append(v)
+            for nei in adj[v]:
+                dfs(nei, res)
+            return res
+        
+        adj = defaultdict(list)
+        for v1, v2 in edges:
+            adj[v1].append(v2)
+            adj[v2].append(v1)
+
+        res = 0
+        visited = set()
+        for v in range(n):
+            if v in visited:
+                continue
+            
+            component = dfs(v, [])
+            flag = True
+            for v2 in component:
+                if len(component) - 1 != len(adj[v2]):
+                    flag = False
+                    break
+            if flag:
+                res += 1
+
+
+        return res
