ed082707d45036ff5fb225bf591c9e2d7e4f7902
Yuan <65935215+Acreast@users.noreply.github.com>
2025-03-31 16:06:12 +0800

    Create file

---
+++ 3394-CheckifGridcanbeCutintoSections/3394-CheckifGridcanbeCutintoSections.py
@@ -0,0 +1,26 @@
+# Last updated: 3/31/2025, 4:06:12 PM
+class Solution:
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        x = [(r[0], r[2]) for r in rectangles]
+        y = [(r[1], r[3]) for r in rectangles]
+
+        x.sort()
+        y.sort()
+        
+        def count_overlapping(intervals):
+            prev_end = -1
+            count = 0
+            for start, end in intervals:
+                if prev_end <= start:
+                    count += 1
+                prev_end = max(prev_end, end)
+
+            return count
+
+        
+        return max(
+            count_overlapping(x),
+            count_overlapping(y)
+        ) >= 3
+        
+
