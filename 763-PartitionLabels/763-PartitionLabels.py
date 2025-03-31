cff65e3b463017cb5f83621b16adfe609d71ded3
Yuan <65935215+Acreast@users.noreply.github.com>
2025-03-31 15:25:11 +0800

    File updated at 3/31/2025, 3:25:11 PM

--- 763-PartitionLabels/763-PartitionLabels.py
+++ 763-PartitionLabels/763-PartitionLabels.py
@@ -1,19 +1,18 @@
-# Last updated: 3/31/2025, 11:23:55 AM
+# Last updated: 3/31/2025, 3:25:11 PM
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         last_index = {}
 
-        for i, n in enumerate(s):
-            last_index[n] = i
-
-        size, end = 0, 0
+        end, size = 0, 0
         res = []
+
+        for i, c in enumerate(s):
+            last_index[c] = i
+        
         for i, c in enumerate(s):
             size += 1
             end = max(end, last_index[c])
-
             if i == end:
                 res.append(size)
                 size = 0
-
         return res
