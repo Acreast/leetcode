6737a5e115db76131300edd7ad7ee0c91dd94e3e
Yuan <65935215+Acreast@users.noreply.github.com>
2025-04-28 23:05:27 +0800

    Create file

---
+++ 2302-CountSubarraysWithScoreLessThanK/version/python3/2302-CountSubarraysWithScoreLessThanK_20250428_230545.py
@@ -0,0 +1,13 @@
+# Last updated: 4/28/2025, 11:05:45 PM
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        count = 0
+        sum = 0
+        left = 0
+        for right in range(len(nums)):
+            sum += nums[right]
+            while sum * (right - left + 1) >= k:
+                sum -= nums[left]
+                left += 1
+            count += (right - left + 1)
+        return count
