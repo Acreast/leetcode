d7ef346f27f8da85a40d5e681f1b01a288d0c951
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-10 00:55:59 +0800

    Create 3439-RescheduleMeetingsforMaximumFreeTimeI

---
+++ 3439-RescheduleMeetingsforMaximumFreeTimeI/3439-RescheduleMeetingsforMaximumFreeTimeI.py
@@ -0,0 +1,18 @@
+# Last updated: 7/10/2025, 12:56:21 AM
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        count = len(startTime)
+        prefixSum = [0] * (count + 1)
+        max_time = 0
+
+        for i in range(count):
+            prefixSum[i + 1] = prefixSum[i] + (endTime[i] - startTime[i])
+        
+        for i in range(k -1, count):
+            occupied = prefixSum[i + 1] - prefixSum[i - k + 1]
+            window_end = eventTime if i == count - 1 else startTime[i + 1]
+            window_start = 0 if i == k - 1 else endTime[i - k]
+            free_time = window_end - window_start - occupied
+            max_time = max(max_time, free_time)
+        
+        return max_time
