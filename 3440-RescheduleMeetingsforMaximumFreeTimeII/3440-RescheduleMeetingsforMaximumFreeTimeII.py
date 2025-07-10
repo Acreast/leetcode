05380262de956dec65a4cc49a16ef085b4f9df5a
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-11 00:02:51 +0800

    Create 3440-RescheduleMeetingsforMaximumFreeTimeII

---
+++ 3440-RescheduleMeetingsforMaximumFreeTimeII/3440-RescheduleMeetingsforMaximumFreeTimeII.py
@@ -0,0 +1,34 @@
+# Last updated: 7/11/2025, 12:03:16 AM
+class Solution:
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+        n = len(startTime)
+        if n == 0:
+            return eventTime
+        
+        gaps = [0] * (n + 1)
+        gaps[0] = startTime[0]
+
+        for i in range(1, n):
+            gaps[i] = startTime[i] - endTime[i-1]
+        gaps[n] = eventTime - endTime[n-1]
+
+        largestRight = [0] *  (n + 1)
+        for i in range(n - 1, -1, -1):
+            largestRight[i] = max(largestRight[i + 1], gaps[i + 1])
+        
+        res = 0
+        largestLeft = 0
+
+        for i in range(1, n + 1):
+            duration = endTime[i - 1] - startTime[i - 1]
+            canFitLeft = largestLeft >= duration
+            canFitRight = largestRight[i] >= duration
+
+            if canFitLeft or canFitRight:
+                merged = gaps[i-1] + gaps[i] + duration
+                res = max(res, merged)
+            
+            res = max(res, gaps[i - 1] + gaps[i])
+            largestLeft = max(largestLeft, gaps[i - 1])
+        
+        return res
