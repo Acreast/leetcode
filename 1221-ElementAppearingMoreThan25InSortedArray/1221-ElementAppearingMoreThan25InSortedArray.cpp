a55fd7c8b66de05174b938f1e4bc8f02b18c716d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:53:02 +0800

    Create 1221-ElementAppearingMoreThan25InSortedArray

---
+++ 1221-ElementAppearingMoreThan25InSortedArray/1221-ElementAppearingMoreThan25InSortedArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/12/2025, 11:53:00 PM
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+                int size = arr.size();
+        int qtr = size / 4;
+        int cnt = 1;
+        int p = arr[0];
+        for (int i = 1 ; i < size ; i++) {
+
+            if ( p == arr[i]) cnt++;
+            else cnt = 1;
+            
+            if (cnt > qtr) return arr[i];
+            
+            p = arr[i];
+        }
+
+        return p;
+    }
+};
