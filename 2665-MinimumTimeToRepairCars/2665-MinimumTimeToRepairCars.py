6062c73ba311a1a9322b70517b73671ac6d8653b
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:45:21 +0800

    Create 2665-MinimumTimeToRepairCars

---
+++ 2665-MinimumTimeToRepairCars/2665-MinimumTimeToRepairCars.py
@@ -0,0 +1,22 @@
+# Last updated: 7/12/2025, 11:45:19 PM
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        
+        def repair_counter(time):
+            count = 0
+            for r in ranks:
+                count += int(sqrt(time / r))
+            return count
+
+        l, r = 1, ranks[0] * cars *cars
+        res = -1
+        while l <= r:
+            m = (l + r) // 2
+            repair_count = repair_counter(m)
+            if repair_count >= cars:
+                res = m
+                r = m - 1
+            else:
+                l = m + 1
+
+        return res
