7f64a785a116bce76220c5fac7ac170af1d6df00
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:46:52 +0800

    Create 2387-PartitionArraySuchThatMaximumDifferenceIsK

---
+++ 2387-PartitionArraySuchThatMaximumDifferenceIsK/2387-PartitionArraySuchThatMaximumDifferenceIsK.py
@@ -0,0 +1,15 @@
+# Last updated: 7/12/2025, 11:46:51 PM
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = 1
+        min_val = nums[0]
+        for num in nums[1:]:
+            if num - min_val > k:
+                ans += 1
+                min_val = num
+        return ans
+            
+
+
+                
