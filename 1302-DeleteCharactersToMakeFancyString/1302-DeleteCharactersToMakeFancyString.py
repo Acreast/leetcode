1a48c97a0b06b96606d85e0929791df0c139b30f
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:52:43 +0800

    Create 1302-DeleteCharactersToMakeFancyString

---
+++ 1302-DeleteCharactersToMakeFancyString/1302-DeleteCharactersToMakeFancyString.py
@@ -0,0 +1,15 @@
+# Last updated: 7/12/2025, 11:52:41 PM
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        res = s[0]
+        cnt = 1
+        for i in range(1,len(s)):
+            if s[i] == res[-1]:
+                cnt += 1
+                if cnt < 3:
+                    res += s[i]
+            else:
+                cnt = 1
+                res += s[i]
+
+        return res
