b51fe70e02d5e5e0565f56a2e1f613fa1cb161df
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:50:41 +0800

    Create 1653-NumberOfGoodLeafNodesPairs

---
+++ 1653-NumberOfGoodLeafNodesPairs/1653-NumberOfGoodLeafNodesPairs.py
@@ -0,0 +1,39 @@
+# Last updated: 7/12/2025, 11:50:39 PM
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        self.result = 0
+        def dfs(node):
+            if not node: return []
+            if not node.left and not node.right: return [1]
+            left, right = dfs(node.left), dfs(node.right)
+  
+            for l in left:
+                if right and l + right[0] > distance: break
+                for r in right:
+                    if l + r > distance: break
+                    self.result += 1
+
+            # merge sort left and right
+            merged = []
+            l, r = 0, 0
+            while l < len(left) and r < len(right):
+                if left[l] <= right[r]:
+                    val = left[l] + 1
+                    l += 1
+                else:
+                    val = right[r] + 1
+                    r += 1
+                if val > distance:
+                    break
+                merged.append(val)
+
+            while l < len(left) and left[l] + 1 < distance:
+                merged.append(left[l] + 1)
+                l += 1
+            while r < len(right) and right[r] + 1 < distance:
+                merged.append(right[r] + 1)
+                r += 1
+            return merged
+        
+        dfs(root)
+        return self.result
