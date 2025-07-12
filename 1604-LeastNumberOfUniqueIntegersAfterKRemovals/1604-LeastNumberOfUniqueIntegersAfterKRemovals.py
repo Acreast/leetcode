9f7b7195095c595b3836b23c5107c8e5ec78234f
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:51:05 +0800

    Create 1604-LeastNumberOfUniqueIntegersAfterKRemovals

---
+++ 1604-LeastNumberOfUniqueIntegersAfterKRemovals/1604-LeastNumberOfUniqueIntegersAfterKRemovals.py
@@ -0,0 +1,14 @@
+# Last updated: 7/12/2025, 11:51:04 PM
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        freq = Counter(arr)
+        heap = list(freq.values())
+        heapq.heapify(heap)
+
+        res = len(heap)
+        while k > 0 and heap:
+            f = heapq.heappop(heap)
+            if k >= f:
+                k -= f
+                res -= 1
+        return res
