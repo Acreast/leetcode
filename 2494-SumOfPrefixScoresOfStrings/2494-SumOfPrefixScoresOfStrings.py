43ad7aaa33dbc724d6a3930391f7f64cd14a391f
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:46:14 +0800

    Create 2494-SumOfPrefixScoresOfStrings

---
+++ 2494-SumOfPrefixScoresOfStrings/2494-SumOfPrefixScoresOfStrings.py
@@ -0,0 +1,17 @@
+# Last updated: 7/12/2025, 11:46:13 PM
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        res = []
+        count = defaultdict(int)
+
+        for w in words:
+            for i in range(len(w)):
+                count[w[:i+1]] += 1
+        
+        for w in words:
+            score = 0
+            for i in range(len(w)):
+                score += count[w[:i+1]]
+            res.append(score)
+
+        return res
