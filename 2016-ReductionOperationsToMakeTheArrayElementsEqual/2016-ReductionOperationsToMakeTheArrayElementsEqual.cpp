30d7030c75662d6da06710aa5e40eaee4dd3dd14
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:48:48 +0800

    Create 2016-ReductionOperationsToMakeTheArrayElementsEqual

---
+++ 2016-ReductionOperationsToMakeTheArrayElementsEqual/2016-ReductionOperationsToMakeTheArrayElementsEqual.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/12/2025, 11:48:47 PM
+class Solution {
+public:
+    int reductionOperations(vector<int>& n) {
+        int res = 0, sz = n.size();
+        sort(begin(n), end(n));
+        for (int j = sz - 1; j > 0; --j)
+            if (n[j - 1] != n[j])
+                res += sz - j;
+        return res;
+    }
+};
