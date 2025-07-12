748d8b7ad2cb62a20a642599d3c473f777cb58b3
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:51:42 +0800

    Create 1496-LuckyNumbersInAMatrix

---
+++ 1496-LuckyNumbersInAMatrix/1496-LuckyNumbersInAMatrix.py
@@ -0,0 +1,17 @@
+# Last updated: 7/12/2025, 11:51:41 PM
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        res = []
+
+        row_min = set()
+        for r in range(ROWS):
+            row_min.add(min(matrix[r]))
+        for c in range(COLS):
+            cur_max = matrix[0][c]
+            for r in range(ROWS):
+                cur_max = max(cur_max, matrix[r][c])
+            if cur_max in row_min:
+                res.append(cur_max)
+        
+        return res
