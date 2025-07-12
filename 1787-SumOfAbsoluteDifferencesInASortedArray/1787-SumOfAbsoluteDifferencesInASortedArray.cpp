17fed1907aafabd77fe120a681a367d661d471a5
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:49:46 +0800

    Create 1787-SumOfAbsoluteDifferencesInASortedArray

---
+++ 1787-SumOfAbsoluteDifferencesInASortedArray/1787-SumOfAbsoluteDifferencesInASortedArray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/12/2025, 11:49:45 PM
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        vector<int>ans(nums.size(),0);
+        for(int i = 1;i<nums.size();i++)
+            ans[0]+=(nums[i]-nums[0]);
+        
+        for(int j = 1;j<nums.size();j++)
+            ans[j] = ans[j-1]+(nums[j]-nums[j-1])*j-(nums[j]-nums[j-1])*(nums.size()-j);
+        
+        return ans;
+    }
+};
