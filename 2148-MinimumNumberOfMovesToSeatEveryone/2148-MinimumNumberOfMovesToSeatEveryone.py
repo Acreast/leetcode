01a061bc2be8782ca63571c840a175c8dc45e937
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:48:11 +0800

    Create 2148-MinimumNumberOfMovesToSeatEveryone

---
+++ 2148-MinimumNumberOfMovesToSeatEveryone/2148-MinimumNumberOfMovesToSeatEveryone.py
@@ -0,0 +1,14 @@
+# Last updated: 7/12/2025, 11:48:10 PM
+class Solution:
+    def minMovesToSeat(self, seats, students):
+        pos = [0] * 101
+        n = len(seats)
+        for i in range(n):
+            pos[seats[i]] += 1
+            pos[students[i]] -= 1
+        res = 0
+        current = 0
+        for i in pos:
+            res += abs(current)
+            current += i
+        return res
