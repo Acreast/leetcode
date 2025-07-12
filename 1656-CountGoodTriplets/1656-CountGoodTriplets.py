c15dcee6f28b37bc676d5b4336c834f1fb715aa4
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:50:39 +0800

    Create 1656-CountGoodTriplets

---
+++ 1656-CountGoodTriplets/1656-CountGoodTriplets.py
@@ -0,0 +1,16 @@
+# Last updated: 7/12/2025, 11:50:38 PM
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        res = 0
+        N = len(arr)
+
+        for i in range(N - 2):
+            for j in range( i + 1, N - 1):
+                for k in range( j + 1, N):
+                    if (
+                        abs(arr[i] - arr[j]) <= a and
+                        abs(arr[j] - arr[k]) <= b and
+                        abs(arr[i] - arr[k]) <= c 
+                    ):
+                        res += 1
+        return res
