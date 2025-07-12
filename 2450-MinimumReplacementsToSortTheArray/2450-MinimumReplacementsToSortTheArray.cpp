e8fcdeff48bba52ea23b6fa510f8f235258c8bcc
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:46:33 +0800

    Create 2450-MinimumReplacementsToSortTheArray

---
+++ 2450-MinimumReplacementsToSortTheArray/2450-MinimumReplacementsToSortTheArray.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/12/2025, 11:46:32 PM
+class Solution {
+public:
+long long minimumReplacement(vector<int>& nums) {
+	int n=nums.size();
+	long long ans=0, prev=nums[n-1];
+	for(int i=n-2;i>=0;i--){
+		int noOfTime=nums[i]/prev;   
+		if((nums[i])%prev!=0){
+			noOfTime++;
+			prev=nums[i]/noOfTime;
+		}   
+		ans+=noOfTime-1;
+	}
+	return ans;
+}
+};
