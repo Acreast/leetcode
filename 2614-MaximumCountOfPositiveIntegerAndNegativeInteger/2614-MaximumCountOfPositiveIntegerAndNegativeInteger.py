1f3188f9305ef80513e426f34f435e4129de483c
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:45:31 +0800

    Create 2614-MaximumCountOfPositiveIntegerAndNegativeInteger

---
+++ 2614-MaximumCountOfPositiveIntegerAndNegativeInteger/2614-MaximumCountOfPositiveIntegerAndNegativeInteger.py
@@ -0,0 +1,12 @@
+# Last updated: 7/12/2025, 11:45:30 PM
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        negative_count = 0
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                negative_count += 1
+                continue
+            if nums[i] == 0:
+                continue
+            return max(negative_count, len(nums) - i)
+        return negative_count       
