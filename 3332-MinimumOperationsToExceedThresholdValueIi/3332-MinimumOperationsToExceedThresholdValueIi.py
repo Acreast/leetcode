4a408eb105ac6ee93df5ef132211f82bbf0a696d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:43:23 +0800

    Create 3332-MinimumOperationsToExceedThresholdValueIi

---
+++ 3332-MinimumOperationsToExceedThresholdValueIi/3332-MinimumOperationsToExceedThresholdValueIi.py
@@ -0,0 +1,14 @@
+# Last updated: 7/12/2025, 11:43:21 PM
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        res = 0
+        
+        while len(nums) > 1 and nums[0] < k:
+            
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            res += 1
+            heapq.heappush(nums, min(x, y) * 2 + max(x, y))
+        
+        return res
