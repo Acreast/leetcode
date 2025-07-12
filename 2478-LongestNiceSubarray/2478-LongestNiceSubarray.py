31b7a29d646a6ac9eb7d970f23da59f2f215eba4
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:46:20 +0800

    Create 2478-LongestNiceSubarray

---
+++ 2478-LongestNiceSubarray/2478-LongestNiceSubarray.py
@@ -0,0 +1,14 @@
+# Last updated: 7/12/2025, 11:46:18 PM
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        res = 0
+        cur = 0
+        l = 0
+        for r in range(len(nums)):
+            while cur & nums[r]:
+                cur = cur ^ nums[l]
+                l += 1
+            res = max(res, r - l + 1)
+            cur = cur | nums[r]
+
+        return res
