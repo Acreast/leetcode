ff6455acf7e7f84b9721e8928a2a8607ad085e27
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:49:53 +0800

    Create 1770-MinimumDeletionsToMakeCharacterFrequenciesUnique

---
+++ 1770-MinimumDeletionsToMakeCharacterFrequenciesUnique/1770-MinimumDeletionsToMakeCharacterFrequenciesUnique.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/12/2025, 11:49:52 PM
+class Solution {
+public:
+    int minDeletions(string s) {
+        int cnt[26] = {}, res = 0;
+        unordered_set<int> used;
+        for (char ch : s)
+            ++cnt[ch - 'a'];
+        for (int i = 0; i < 26; ++i)
+            for (; cnt[i] > 0 && !used.insert(cnt[i]).second; --cnt[i])
+                ++res;
+        return res;
+    }
+};
