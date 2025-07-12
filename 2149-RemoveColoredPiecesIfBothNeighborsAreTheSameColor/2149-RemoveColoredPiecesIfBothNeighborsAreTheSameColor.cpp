b66ccbe4bb1028c6c3a2e1050c9957c481955c03
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:48:07 +0800

    Create 2149-RemoveColoredPiecesIfBothNeighborsAreTheSameColor

---
+++ 2149-RemoveColoredPiecesIfBothNeighborsAreTheSameColor/2149-RemoveColoredPiecesIfBothNeighborsAreTheSameColor.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/12/2025, 11:48:06 PM
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        map<char, int> c;
+        for (auto it = colors.begin(); it != colors.end(); ) {
+            char x = *it;
+            auto t = it;
+            while (t != colors.end() && *t == x) {
+                t++;
+            }
+            c[x] += max(int(distance(it, t) - 2), 0);
+            it = t;
+        }
+
+        if (c['A'] > c['B']) {
+            return true;
+        }
+        return false;
+    }
+};
