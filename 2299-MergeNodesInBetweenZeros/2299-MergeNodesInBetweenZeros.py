8143702aaa2cc2ecb9fb1ef8f3956b4909db79cc
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:47:14 +0800

    Create 2299-MergeNodesInBetweenZeros

---
+++ 2299-MergeNodesInBetweenZeros/2299-MergeNodesInBetweenZeros.py
@@ -0,0 +1,22 @@
+# Last updated: 7/12/2025, 11:47:13 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        tail = dummy = ListNode()
+
+        while cur.next:
+            node = ListNode(0)
+
+            while cur.next.val != 0:
+                node.val += cur.next.val
+                cur = cur.next
+            tail.next = node
+            tail = tail.next
+            cur = cur.next
+        
+        return dummy.next
