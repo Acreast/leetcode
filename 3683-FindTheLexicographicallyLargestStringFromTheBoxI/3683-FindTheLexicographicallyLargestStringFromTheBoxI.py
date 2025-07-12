04483c44fa79c14c8c9e48ea23e4245d7298cf64
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:42:25 +0800

    Create 3683-FindTheLexicographicallyLargestStringFromTheBoxI

---
+++ 3683-FindTheLexicographicallyLargestStringFromTheBoxI/3683-FindTheLexicographicallyLargestStringFromTheBoxI.py
@@ -0,0 +1,12 @@
+# Last updated: 7/12/2025, 11:42:23 PM
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        res = ""
+        length = len(word) - numFriends + 1
+        for i in range(0, len(word)):
+            temp = word[i:i + length]
+            if temp > res:
+                res = temp
+        return res
