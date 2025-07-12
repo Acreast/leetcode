8a79a59eed4e80d3e8eb177af360b233b81fdadf
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:43:13 +0800

    Create 3394-MinimumArrayEnd

---
+++ 3394-MinimumArrayEnd/3394-MinimumArrayEnd.py
@@ -0,0 +1,14 @@
+# Last updated: 7/12/2025, 11:43:12 PM
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        result = x
+        remaining = n - 1
+        position = 1
+    
+        while remaining:
+            if not (x & position):
+                result |= (remaining & 1) * position
+                remaining >>= 1
+            position <<= 1
+    
+        return result
