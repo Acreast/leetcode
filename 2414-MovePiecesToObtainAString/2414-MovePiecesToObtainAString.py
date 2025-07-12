213717ec471f48e2710b75ac3d266a654e24c9fc
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:46:46 +0800

    Create 2414-MovePiecesToObtainAString

---
+++ 2414-MovePiecesToObtainAString/2414-MovePiecesToObtainAString.py
@@ -0,0 +1,19 @@
+# Last updated: 7/12/2025, 11:46:44 PM
+class Solution:
+    def canChange(self, s: str, t: str) -> bool:
+        n=len(s)
+        s+='@'
+        t+='@'
+        i, j=0, 0
+        while i<n or j<n:
+            while i<n and s[i]=='_': i+=1
+            while j<n and t[j]=='_': j+=1
+            c=s[i]
+            if c!=t[j]: return False
+            if c=='L' and i<j: return False
+            if c=='R' and i>j: return False
+            i+=1
+            j+=1
+        return True
+        
+        
