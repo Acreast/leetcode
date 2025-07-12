56d1fe287397e358bc5aabe0ba012f7f3b0b7d9f
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:45:35 +0800

    Create 2599-TakeKOfEachCharacterFromLeftAndRight

---
+++ 2599-TakeKOfEachCharacterFromLeftAndRight/2599-TakeKOfEachCharacterFromLeftAndRight.py
@@ -0,0 +1,22 @@
+# Last updated: 7/12/2025, 11:45:34 PM
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        # Total counts
+        count = [0, 0, 0]
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+
+        if min(count) < k:
+            return -1
+
+        # Sliding Window
+        res = float("inf")
+        l = 0
+        for r in range(len(s)):
+            count[ord(s[r]) - ord('a')] -= 1
+
+            while min(count) < k:
+                count[ord(s[l]) - ord('a')] += 1
+                l += 1
+            res = min(res, len(s) - (r - l + 1))
+        return res
