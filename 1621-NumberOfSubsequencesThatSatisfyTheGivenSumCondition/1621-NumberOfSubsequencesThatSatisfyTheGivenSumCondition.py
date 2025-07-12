aa4abcc2f65e8addf86a8dd55b38c3935a541199
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:50:50 +0800

    Create 1621-NumberOfSubsequencesThatSatisfyTheGivenSumCondition

---
+++ 1621-NumberOfSubsequencesThatSatisfyTheGivenSumCondition/1621-NumberOfSubsequencesThatSatisfyTheGivenSumCondition.py
@@ -0,0 +1,16 @@
+# Last updated: 7/12/2025, 11:50:49 PM
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        res = 0
+        r = len(nums) - 1
+        MOD = 10** 9 + 7
+        for i, l in enumerate(nums):
+            while (nums[r] + l) > target and i <= r:
+                r -= 1
+            if i <= r:
+                res += (2 ** ( r - i))
+                res %= MOD
+
+        return res
+        
