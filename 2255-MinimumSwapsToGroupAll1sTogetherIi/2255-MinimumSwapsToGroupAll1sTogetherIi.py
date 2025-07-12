4ad732ec2c13cb5e49afddf80574bd2ef8d53d28
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:47:26 +0800

    Create 2255-MinimumSwapsToGroupAll1sTogetherIi

---
+++ 2255-MinimumSwapsToGroupAll1sTogetherIi/2255-MinimumSwapsToGroupAll1sTogetherIi.py
@@ -0,0 +1,11 @@
+# Last updated: 7/12/2025, 11:47:25 PM
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        k = nums.count(1)
+        mx = cnt = sum(nums[:k])
+        n = len(nums)
+        for i in range(k, n + k):
+            cnt += nums[i % n]
+            cnt -= nums[(i - k + n) % n]
+            mx = max(mx, cnt)
+        return k - mx
