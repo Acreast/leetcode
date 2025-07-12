09519827c0164cf128bbd0fef4f07e1a9dedc38d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:53:16 +0800

    Create 1160-LetterTilePossibilities

---
+++ 1160-LetterTilePossibilities/1160-LetterTilePossibilities.py
@@ -0,0 +1,16 @@
+# Last updated: 7/12/2025, 11:53:14 PM
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        
+        count = Counter(tiles)
+        def backtrack():
+            res = 0
+            for c in count:
+                if count[c] > 0:
+                    res += 1
+                    count[c] -= 1
+                    res += backtrack()
+                    count[c] += 1
+            return res
+
+        return backtrack()
