a766c954ce96ba4970754872ad4894074450c40d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:50:20 +0800

    Create 1715-SplitAStringIntoTheMaxNumberOfUniqueSubstrings

---
+++ 1715-SplitAStringIntoTheMaxNumberOfUniqueSubstrings/1715-SplitAStringIntoTheMaxNumberOfUniqueSubstrings.py
@@ -0,0 +1,18 @@
+# Last updated: 7/12/2025, 11:50:19 PM
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        
+        def dfs(i, cur_set):
+            if i == len(s):
+                return 0
+            res = 0
+            for j in range(i, len(s)):
+                sub_str = s[i:j+1]
+                if sub_str in cur_set:
+                    continue
+                cur_set.add(sub_str)
+                res = max(res, 1 + dfs(j + 1, cur_set))
+                cur_set.remove(sub_str)
+            return res
+
+        return dfs(0, set())
