7e8f64182132fb52f7589aae14b1d2580698cac3
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:50:06 +0800

    Create 1744-NumberOfWaysToFormATargetStringGivenADictionary

---
+++ 1744-NumberOfWaysToFormATargetStringGivenADictionary/1744-NumberOfWaysToFormATargetStringGivenADictionary.py
@@ -0,0 +1,15 @@
+# Last updated: 7/12/2025, 11:50:05 PM
+class Solution:
+    def numWays(self, a: List[str], t: str) -> int:
+        z = [*map(Counter, zip(*a))]
+
+        @cache
+        def f(i, j):
+            if i == len(t):
+                return 1
+            if j < len(a[0]):
+                return z[j][t[i]]*f(i+1,j+1) + f(i,j+1)
+            
+            return 0
+        
+        return f(0, 0)%(10**9+7)
