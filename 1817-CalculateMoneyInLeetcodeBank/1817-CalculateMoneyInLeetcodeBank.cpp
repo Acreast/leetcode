1ba795f2319f47bb92d49b73018a0801d9715625
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:49:38 +0800

    Create 1817-CalculateMoneyInLeetcodeBank

---
+++ 1817-CalculateMoneyInLeetcodeBank/1817-CalculateMoneyInLeetcodeBank.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/12/2025, 11:49:36 PM
+class Solution {
+public:
+    int totalMoney(int n) {
+                int w = n / 7;
+        int money = w * 28;
+        money += (7 * w * (w - 1)) / 2;
+
+        if (n % 7) {
+            int extra_days = n % 7;
+            int money_to_add = w + 1;
+            for (int i = 0; i < extra_days; ++i) {
+                money += money_to_add;
+                money_to_add += 1;
+            }
+        }
+        return money;
+    }
+};
