6069ee2f978c4446ba7d7cbf25263772cda5db8d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:54:56 +0800

    Create 868-PushDominoes

---
+++ 868-PushDominoes/868-PushDominoes.py
@@ -0,0 +1,20 @@
+# Last updated: 7/12/2025, 11:54:55 PM
+class Solution(object):
+    def pushDominoes(self, s):
+        s = 'L' + s + 'R'
+        res = ''
+        prev = 0
+        for curr in range(1, len(s)):
+            if s[curr] == '.':
+                continue
+            span = curr - prev - 1
+            if prev > 0:
+                res += s[prev]
+            if s[prev] == s[curr]:
+                res += s[prev] * span
+            elif s[prev] == 'L' and s[curr] == 'R':
+                res += '.' * span
+            else:
+                res += 'R' * (span // 2) + '.' * (span % 2) + 'L' * (span // 2)
+            prev = curr
+        return res
