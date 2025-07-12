218f01241fe9cc24bdf4ba882ba9b09e149afda4
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:52:10 +0800

    Create 1407-GroupThePeopleGivenTheGroupSizeTheyBelongTo

---
+++ 1407-GroupThePeopleGivenTheGroupSizeTheyBelongTo/1407-GroupThePeopleGivenTheGroupSizeTheyBelongTo.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/12/2025, 11:52:09 PM
+class Solution {
+public:
+vector<vector<int>> groupThePeople(vector<int>& gz) {
+  vector<vector<int>> res, groups(gz.size() + 1);
+  for (auto i = 0; i < gz.size(); ++i) {
+    groups[gz[i]].push_back(i);
+    if (groups[gz[i]].size() == gz[i]) {
+      res.push_back({});
+      swap(res.back(), groups[gz[i]]);
+    }
+  }
+  return res;
+}
+};
