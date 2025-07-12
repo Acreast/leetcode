caf35e202792ae940dc37b33fe53fe5f389a68d1
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:54:10 +0800

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/12/2025, 11:54:09 PM
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        
+        auto helper = [&](int goal) {
+            if (goal < 0) {
+                return 0;
+            }
+
+            int result = 0;
+            int left = 0;
+            int current = 0;
+            for (int r = 0; r < nums.size(); r++) {
+                current += nums[r];
+
+                while (current > goal) {
+                    current -= nums[left];
+                    left += 1;
+                }
+                result += (r - left + 1); 
+            }
+            return result;
+        };
+        return (helper(goal) - helper(goal - 1));
+    }
+};
