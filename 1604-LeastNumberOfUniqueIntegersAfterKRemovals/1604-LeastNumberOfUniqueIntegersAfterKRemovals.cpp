75ba749e68d3b1730d12cb687d744e8968385f29
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-12 23:51:00 +0800

    Create 1604-LeastNumberOfUniqueIntegersAfterKRemovals

---
+++ 1604-LeastNumberOfUniqueIntegersAfterKRemovals/1604-LeastNumberOfUniqueIntegersAfterKRemovals.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/12/2025, 11:50:59 PM
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(std::vector<int>& arr, int k) {
+        std::unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+
+        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+        for (const auto& entry : freq) {
+            minHeap.push(entry.second);
+        }
+
+        int res = minHeap.size();
+        while (k > 0 && !minHeap.empty()) {
+            int f = minHeap.top();
+            minHeap.pop();
+
+            if (k >= f) {
+                k -= f;
+                res--;
+            }
+        }
+
+        return res;
+    }
+};
