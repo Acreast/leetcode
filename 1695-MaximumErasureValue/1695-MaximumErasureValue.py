80f9148d2c0704b405f72c9743e8f4cc97df698e
Yuan <65935215+Acreast@users.noreply.github.com>
2025-07-22 20:25:30 +0800

    File updated at 7/22/2025, 8:25:32 PM

--- 1695-MaximumErasureValue/1695-MaximumErasureValue.py
+++ 1695-MaximumErasureValue/1695-MaximumErasureValue.py
@@ -1,18 +1,17 @@
-# Last updated: 7/22/2025, 7:55:30 PM
+# Last updated: 7/22/2025, 8:25:32 PM
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         l = 0
-        r = 0
         res = 0
         num_set = set()
-        current_sum = 0
+        cur_sum = 0
         for r in range(len(nums)):
             while nums[r] in num_set:
-                current_sum -= nums[l]
+                cur_sum -= nums[l]
                 num_set.remove(nums[l])
                 l += 1
-            current_sum += nums[r]    
+            
+            cur_sum += nums[r]
             num_set.add(nums[r])
-            res = max(res, current_sum)
-        return res
-                
+            res = max(cur_sum, res)
+        return res        
