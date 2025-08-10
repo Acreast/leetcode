6a897acacb0085cf4bcdd4c9f789bcee7994a6a2
Yuan <65935215+Acreast@users.noreply.github.com>
2025-08-10 18:54:56 +0800

    File updated at 8/10/2025, 6:55:07 PM

--- 231-PowerofTwo/231-PowerofTwo.py
+++ 231-PowerofTwo/231-PowerofTwo.py
@@ -1,4 +1,22 @@
-# Last updated: 8/9/2025, 6:46:16 PM
+# Last updated: 8/10/2025, 6:55:07 PM
 class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and not (n & (n - 1))
+    def reorderedPowerOf2(self, n: int) -> bool:
+        def generate_numbers(n):
+            digits = str(n)
+            seen = set()
+
+            for perm in permutations(digits):
+                if perm[0] != '0':  # no leading zero
+                    num = int(''.join(perm))
+                    seen.add(num)
+
+            return sorted(seen)
+        
+
+        num_set = generate_numbers(n)
+        for num in num_set:
+            if num > 0 and not (num & (num - 1)):
+                return True
+            
+        
+        return False
