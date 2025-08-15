787165cfdc2d273d17aa47c1d5f8b55d834f063c
Yuan <65935215+Acreast@users.noreply.github.com>
2025-08-15 23:18:52 +0800

    File updated at 8/15/2025, 11:19:00 PM

--- 2264-Largest3-Same-DigitNumberinString/2264-Largest3-Same-DigitNumberinString.py
+++ 2264-Largest3-Same-DigitNumberinString/2264-Largest3-Same-DigitNumberinString.py
@@ -1,11 +1,13 @@
-# Last updated: 8/14/2025, 10:51:48 PM
+# Last updated: 8/15/2025, 11:19:00 PM
 class Solution:
-    def largestGoodInteger(self, num: str) -> str:
-        res = ""
-        i = 0
-        for i in range(len(num) - 2):
-            if num[i] == num[i + 1] == num[i + 2]:
-                temp = f"{num[i]}{num[i+1]}{num[i+2]}"
-                res = max(temp, res)
-
-        return res
+    def isPowerOfFour(self, n: int) -> bool:
+        cur = 4
+        if n == 1:
+            return True
+        while cur <= n:
+            
+            if cur == n:
+                return True
+            cur = cur * 4
+        
+        return False
