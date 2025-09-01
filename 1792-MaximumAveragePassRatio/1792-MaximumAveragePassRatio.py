16b30e989305c16931d236b3d42649c87e048f6b
Yuan <65935215+Acreast@users.noreply.github.com>
2025-09-02 00:21:39 +0800

    File updated at 9/2/2025, 12:21:53 AM

--- 1792-MaximumAveragePassRatio/1792-MaximumAveragePassRatio.py
+++ 1792-MaximumAveragePassRatio/1792-MaximumAveragePassRatio.py
@@ -1,19 +1,20 @@
-# Last updated: 9/2/2025, 12:12:06 AM
+# Last updated: 9/2/2025, 12:21:53 AM
 class Solution:
-    def maxAverageRatio(self, classes: List[List[int]], k: int) -> float:
-        n=len(classes)
-        sum=0
-        A=[]
-        for p,q in classes:
-            sum+=p/q
-            A.append(((p-q)/(q*(q+1)), p, q)) # change sign
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        
+        heap = []
+        sum = 0
+        for passed, total in classes:
+            sum += passed/total
+            heap.append(((passed - total)/(total * (total + 1)), passed, total))
+        
+        heapq.heapify(heap)
 
-        heapify(A)
-
-        for _ in range(k):
-            (r, p, q)=A[0]
-            if r==0: break
-            sum-=r # change sign
-            r2=(p-q)/((q +1.0)* (q + 2.0))
-            heapreplace(A, (r2, p+1, q+1))
-        return sum/n
+        for _ in range(extraStudents):
+            (avg, passed, total) = heap[0]
+            if avg == 0:
+                break
+            sum -= avg
+            new_avg = (passed - total) / ((total + 1.0) * (total + 2.0))
+            heapreplace(heap, (new_avg, passed + 1, total + 1))
+        return sum/len(classes)  
