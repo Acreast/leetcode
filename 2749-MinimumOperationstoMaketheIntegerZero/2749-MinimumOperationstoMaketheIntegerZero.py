07c73477756d41a0444695112a6a9b9d59640e59
Yuan <65935215+Acreast@users.noreply.github.com>
2025-09-06 22:56:25 +0800

    File updated at 9/6/2025, 10:56:24 PM

--- 2749-MinimumOperationstoMaketheIntegerZero/2749-MinimumOperationstoMaketheIntegerZero.py
+++ 2749-MinimumOperationstoMaketheIntegerZero/2749-MinimumOperationstoMaketheIntegerZero.py
@@ -1,11 +1,25 @@
-# Last updated: 9/5/2025, 11:41:39 PM
+# Last updated: 9/6/2025, 10:56:24 PM
 class Solution:
-    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
-        
-        for i in range(1, 61):
-            x = num1 - num2 * i
-            if x < i:
-                return - 1
-            if i >= x.bit_count():
-                return i
-        return -1
+    def minOperations(self, queries: List[List[int]]) -> int:
+        def helper(num):
+            if num <= 0:
+                return 0
+            if num <= 3:
+                return num
+            res = 0
+            base = 1
+            step = 1
+
+            while base <= num:
+                count = min(num, base * 4 - 1) - base + 1
+                res += count * step
+                base *= 4
+                step += 1
+            return res
+
+
+        res = 0
+        for l, r in queries:
+            total = helper(r) - helper(l-1)
+            res += (total + 1) // 2
+        return res
