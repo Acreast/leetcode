f3eb558c4a9b874acf80978fc9c43e0f0577435d
Yuan <65935215+Acreast@users.noreply.github.com>
2025-09-15 22:47:16 +0800

    Create 1935-MaximumNumberofWordsYouCanType

---
+++ 1935-MaximumNumberofWordsYouCanType/1935-MaximumNumberofWordsYouCanType.py
@@ -0,0 +1,17 @@
+# Last updated: 9/15/2025, 10:47:30 PM
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        
+        word_arr = text.split(" ")
+        res = 0
+        for word in word_arr:
+            has_broken = False
+            for c in brokenLetters:
+                if c in word:
+                    has_broken = True
+            
+            if not has_broken:
+                res += 1
+            
+        return res
+            
