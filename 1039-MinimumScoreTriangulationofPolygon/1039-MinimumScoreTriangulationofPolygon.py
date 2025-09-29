aeeed45ee261f68a2d90d580d0ac4d7d73b4931a
Yuan <65935215+Acreast@users.noreply.github.com>
2025-09-29 23:46:55 +0800

    File updated at 9/29/2025, 11:46:55 PM

--- 1039-MinimumScoreTriangulationofPolygon/1039-MinimumScoreTriangulationofPolygon.py
+++ 1039-MinimumScoreTriangulationofPolygon/1039-MinimumScoreTriangulationofPolygon.py
@@ -1,4 +1,4 @@
-# Last updated: 9/29/2025, 11:45:33 PM
+# Last updated: 9/29/2025, 11:46:55 PM
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
         dp = [[0] * 50 for _ in range(50)]
@@ -10,10 +10,10 @@ class Solution:
                 return dp[i][j]
             for k in range(i + 1, j):
                 res = min(
-                    res if res !=0 else float('inf'),
-                    top_down(i, k, 0) +
+                    res if res != 0 else float('inf'),
+                    top_down(i, k, 0) +  
                     values[i] * values[k] * values[j] +
-                    top_down(k, j, 0)
+                    top_down(k, j, 0)    
                 )
             dp[i][j] = res
             return dp[i][j]
