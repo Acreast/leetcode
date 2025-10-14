880ac28e3935d9f2ccfafd1a2dd6120d0c535042
Yuan <65935215+Acreast@users.noreply.github.com>
2025-10-14 22:32:52 +0800

    File updated at 10/14/2025, 10:33:18 PM

--- 3349-AdjacentIncreasingSubarraysDetectionI/3349-AdjacentIncreasingSubarraysDetectionI.py
+++ 3349-AdjacentIncreasingSubarraysDetectionI/3349-AdjacentIncreasingSubarraysDetectionI.py
@@ -1,17 +1,23 @@
-# Last updated: 10/14/2025, 9:57:26 PM
+# Last updated: 10/14/2025, 10:33:18 PM
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
         size = 1
         prev_size = 0
-        max_size = 0
+        best = 0
         for i in range(1, len(nums)):
             if nums[i] > nums[i - 1]:
                 size += 1
             else:
                 prev_size = size
                 size = 1
-            max_size = max(max_size, max(size >> 1, min(prev_size, size)))
-            if max_size >= k:
+            
+            split_case = size // 2
+
+            adjacent_case = min(prev_size, size)
+
+            best = max(best, split_case, adjacent_case)
+
+            if best >= k:
                 return True
         return False
 
