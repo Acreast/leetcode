0d04467c024a64c5ae0b019c6a155720aeb3a8fe
Yuan <65935215+Acreast@users.noreply.github.com>
2025-10-26 20:19:59 +0800

    File updated at 10/26/2025, 8:20:41 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.py
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -1,10 +1,11 @@
-# Last updated: 10/26/2025, 8:20:23 PM
+# Last updated: 10/26/2025, 8:20:41 PM
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         target_arr = []
         for row in matrix:
             if target >= row[0] and target <= row[-1]:
                 target_arr = row
+                break
         
         l = 0
         r = len(target_arr) - 1
