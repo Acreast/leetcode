7f18c8ab58485c4385260f83eda157dea98ca4e5
Yuan <65935215+Acreast@users.noreply.github.com>
2025-10-26 19:50:41 +0800

    Create 2043-SimpleBankSystem

---
+++ 2043-SimpleBankSystem/2043-SimpleBankSystem.py
@@ -0,0 +1,35 @@
+# Last updated: 10/26/2025, 7:51:24 PM
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.accounts = defaultdict(int)
+        for i,b in enumerate(balance):
+            self.accounts[i + 1] = b
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 not in self.accounts.keys() or account2 not in self.accounts.keys():
+            return False
+        if self.accounts[account1] < money:
+            return False
+        self.accounts[account1] -= money
+        self.accounts[account2] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account not in self.accounts.keys():
+            return False
+        self.accounts[account] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account not in self.accounts.keys() or self.accounts[account] < money:
+            return False
+        self.accounts[account] -= money
+        return True
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
