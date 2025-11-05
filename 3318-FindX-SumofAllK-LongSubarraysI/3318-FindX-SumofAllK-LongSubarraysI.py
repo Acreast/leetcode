5c6826d8ace05e099204c7f647e939a593d22904
Yuan <65935215+Acreast@users.noreply.github.com>
2025-11-06 00:41:28 +0800

    File updated at 11/6/2025, 12:41:34 AM

--- 3318-FindX-SumofAllK-LongSubarraysI/3318-FindX-SumofAllK-LongSubarraysI.py
+++ 3318-FindX-SumofAllK-LongSubarraysI/3318-FindX-SumofAllK-LongSubarraysI.py
@@ -1,18 +1,64 @@
-# Last updated: 11/5/2025, 12:18:29 AM
+# Last updated: 11/6/2025, 12:41:34 AM
 class Solution:
-    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        res = []
+    def findXSum(self, nums, k, x):
+        freq = Counter()
+        top = SortedList()
+        rest = SortedList()
+        top_sum = 0
+        ans = []
 
-        for i in range(len(nums) - k + 1 ):
-            count = Counter(nums[i:i+k])
-            if (len(count)) <= x:
-                res.append(sum(nums[i:i+k]))
-            else:    
-                num_list = list(count.items())
-                num_list.sort(key = lambda p:(p[1], p[0]), reverse = True)
-                cur_count = 0
-                for num_count, num in num_list[:x]:
-                    cur_count += num_count * num
-                res.append(cur_count)
+        def balance():# keeps top and rest balanced
+            nonlocal top_sum
+            while len(top) < x and rest:
+                f, v = rest.pop()
+                top.add((f, v))
+                top_sum += f * v
+            while len(top) > x:
+                f, v = top.pop(0)
+                top_sum -= f * v
+                rest.add((f, v))
+            while rest and top and rest[-1] > top[0]:
+                f1, v1 = rest.pop()
+                f2, v2 = top.pop(0)
+                top_sum += f1 * v1 - f2 * v2
+                top.add((f1, v1))
+                rest.add((f2, v2))
 
-        return res
+        def add(num):
+            nonlocal top_sum
+            old = (freq[num], num)
+            if old in top:
+                top.remove(old)
+                top_sum -= old[0] * old[1]
+            elif old in rest:
+                rest.remove(old)
+            freq[num] += 1
+            new = (freq[num], num)
+            rest.add(new)
+            balance()
+
+        def remove(num):
+            nonlocal top_sum
+            old = (freq[num], num)
+            if old in top:
+                top.remove(old)
+                top_sum -= old[0] * old[1]
+            else:
+                rest.remove(old)
+            freq[num] -= 1
+            if freq[num] > 0:
+                rest.add((freq[num], num))
+            else:
+                del freq[num]
+            balance()
+
+        for i in range(k):# initial window
+            add(nums[i])
+        ans.append(top_sum)
+
+        for i in range(k, len(nums)):# slide the window
+            remove(nums[i - k])
+            add(nums[i])
+            ans.append(top_sum)
+
+        return ans
