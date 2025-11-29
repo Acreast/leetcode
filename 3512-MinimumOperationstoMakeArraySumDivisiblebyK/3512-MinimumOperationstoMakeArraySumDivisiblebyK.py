8fb48e8f78391bd46f5aa660ca499da9135ed11e
Yuan <65935215+Acreast@users.noreply.github.com>
2025-11-29 16:15:27 +0800

    File updated at 11/29/2025, 4:15:35 PM

--- 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.py
+++ 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.py
@@ -1,9 +1,8 @@
-# Last updated: 11/29/2025, 1:05:41 PM
+# Last updated: 11/29/2025, 4:15:35 PM
 1class Solution:
 2    def minOperations(self, nums: List[int], k: int) -> int:
-3        total = sum(nums)
-4        remainder = total % k
-5
-6        return remainder
-7
-8        
+3        
+4
+5        return sum(nums) % k 
+6
+7        
