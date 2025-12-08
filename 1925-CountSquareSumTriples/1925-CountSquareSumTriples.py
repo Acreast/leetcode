d914a4dea6df2c41fc0ba91c1e6426a7228c1202
Yuan <65935215+Acreast@users.noreply.github.com>
2025-12-08 20:48:54 +0800

    File updated at 12/8/2025, 8:49:06 PM

--- 1925-CountSquareSumTriples/1925-CountSquareSumTriples.py
+++ 1925-CountSquareSumTriples/1925-CountSquareSumTriples.py
@@ -1,11 +1,10 @@
-# Last updated: 12/8/2025, 8:48:09 PM
+# Last updated: 12/8/2025, 8:49:06 PM
 1class Solution:
 2    def countTriples(self, n: int) -> int:
-3        res = 0
-4        for a in range(1, n+1):
-5            for b in range(1, n+1):
-6                c2 = a*a + b*b
-7                c = int(c2**0.5)
-8                if c <= n and c*c == c2:
-9                    res += 1
-10        return res
+3        squares = {i*i: i for i in range(1, n+1)}
+4        res = 0
+5        for a in range(1, n+1):
+6            for b in range(1, n+1):
+7                if a*a + b*b in squares:
+8                    res += 1
+9        return res
