136c46a823bd755934f55699ec16451e1772ef23
Yuan <65935215+Acreast@users.noreply.github.com>
2025-12-21 17:07:45 +0800

    File updated at 12/21/2025, 5:08:19 PM

--- 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.py
+++ 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.py
@@ -1,13 +1,30 @@
-# Last updated: 12/20/2025, 1:38:26 PM
+# Last updated: 12/21/2025, 5:08:19 PM
 1class Solution:
 2    def minDeletionSize(self, strs: List[str]) -> int:
-3        res = 0
-4        for c in range(len(strs[0])):
-5            for s in range(1, len(strs)):
-6                if ord(strs[s][c]) < ord(strs[s - 1][c]):
-7                    res += 1
-8                    break
-9        return res
-10        
-11            
-12
+3        n = len(strs)
+4        m = len(strs[0])
+5        res = 0
+6
+7        duplicate = [True] * (n - 1)
+8
+9        for col in range(m):
+10            delete = False
+11
+12            for i in range(n - 1):
+13                if duplicate[i] and strs[i][col] > strs[i + 1][col]:
+14                    delete = True
+15                    break
+16
+17            if delete:
+18                res += 1
+19                continue
+20
+21            for i in range(n - 1):
+22                if duplicate[i] and strs[i][col] < strs[i + 1][col]:
+23                    duplicate[i] = False
+24
+25            # Optimization: if no duplicates remain, we're done
+26            if not any(duplicate):
+27                break
+28
+29        return res
