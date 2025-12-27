e38391921fc5d6b882fc9a4c3b12ae611a0574fc
Yuan <65935215+Acreast@users.noreply.github.com>
2025-12-28 01:34:37 +0800

    File updated at 12/28/2025, 1:34:39 AM

--- 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
@@ -1,23 +1,23 @@
-# Last updated: 7/12/2025, 12:13:53 AM
-class Solution:
-    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        room_availability_time = [0] * n
-        meeting_count = [0] * n
-        for start, end in sorted(meetings):
-            min_room_availability_time = inf
-            min_available_time_room = 0
-            found_unused_room = False
-            for i in range(n):
-                if room_availability_time[i] <= start:
-                    found_unused_room = True
-                    meeting_count[i] += 1
-                    room_availability_time[i] = end
-                    break
-                if min_room_availability_time > room_availability_time[i]:
-                    min_room_availability_time = room_availability_time[i]
-                    min_available_time_room = i
-            if not found_unused_room:
-                room_availability_time[min_available_time_room] += end - start
-                meeting_count[min_available_time_room] += 1
-
-        return meeting_count.index(max(meeting_count))
+# Last updated: 12/28/2025, 1:34:39 AM
+1class Solution:
+2    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+3        room_availability_time = [0] * n
+4        meeting_count = [0] * n
+5        for start, end in sorted(meetings):
+6            min_room_availability_time = inf
+7            min_available_time_room = 0
+8            found_unused_room = False
+9            for i in range(n):
+10                if room_availability_time[i] <= start:
+11                    found_unused_room = True
+12                    meeting_count[i] += 1
+13                    room_availability_time[i] = end
+14                    break
+15                if min_room_availability_time > room_availability_time[i]:
+16                    min_room_availability_time = room_availability_time[i]
+17                    min_available_time_room = i
+18            if not found_unused_room:
+19                room_availability_time[min_available_time_room] += end - start
+20                meeting_count[min_available_time_room] += 1
+21
+22        return meeting_count.index(max(meeting_count))
