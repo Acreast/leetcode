468edfc4d299ed46df5b0dae704ffa40e1af6d02
Yuan <65935215+Acreast@users.noreply.github.com>
2026-01-14 00:27:07 +0800

    Create 3453-SeparateSquaresI

---
+++ 3453-SeparateSquaresI/3453-SeparateSquaresI.py
@@ -0,0 +1,30 @@
+# Last updated: 1/14/2026, 12:27:22 AM
+1class Solution:
+2    def separateSquares(self, squares: List[List[int]]) -> float:
+3        l = float('inf')
+4        r = float('-inf')
+5        total_area = 0
+6        
+7        for x, y, length in squares:
+8            total_area += length * length
+9            l = min(l, y)
+10            r = max(r, y + length)
+11
+12        target_area = total_area / 2.0
+13
+14        for i in range(60):
+15            mid = (l + r ) / 2.0
+16
+17            cur_area = 0
+18            for _ , y, length in squares:
+19                cur_y = max(0, min(length, mid - y))
+20                cur_area += length * cur_y
+21            
+22            if cur_area < target_area:
+23                l = mid
+24            else:
+25                r = mid
+26        
+27        return mid
+28
+29            
