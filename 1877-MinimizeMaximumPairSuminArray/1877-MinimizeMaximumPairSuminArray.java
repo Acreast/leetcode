9436a81c64528ec297547c9f8802fc4cb707fdbc
Yuan <65935215+Acreast@users.noreply.github.com>
2026-01-24 12:20:06 +0800

    File updated at 1/24/2026, 12:20:32 PM

--- 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -1,14 +1,16 @@
-// Last updated: 1/24/2026, 12:18:43 PM
+// Last updated: 1/24/2026, 12:20:32 PM
 1import java.lang.Math;
 2
 3class Solution {
 4    public int minPairSum(int[] nums) {
 5        Arrays.sort(nums);
-6        int n = nums.length;
-7        int res = 0;
-8        for (int i = 0; i < n/2; ++i ) {
-9            res = Math.max(res, (nums[i] + nums[n - i - 1]));
-10        }
-11        return res;
-12    }
-13}
+6        int res = 0;
+7        int n = nums.length;
+8
+9        for (int i = 0; i < n/2; i++) {
+10            res = Math.max(res, nums[i] + nums[n - i - 1]);
+11        }
+12
+13        return res;
+14    }
+15}
