be3934e10b014991979b15c5ffed74c387dfca76
Yuan <65935215+Acreast@users.noreply.github.com>
2026-01-31 23:39:13 +0800

    File updated at 1/31/2026, 11:39:12 PM

--- 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.java
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.java
@@ -1,16 +1,16 @@
-// Last updated: 1/31/2026, 11:23:07 PM
+// Last updated: 1/31/2026, 11:39:12 PM
 1class Solution {
 2    public char nextGreatestLetter(char[] letters, char target) {
-3        int smallest_ord = Integer.MAX_VALUE;
-4        char res = letters[0];
+3        char res = letters[0];
+4        boolean threshold_reached = false;
 5        for(char c : letters) {
-6            int ord = c - target;
-7            if (ord < 1) {
-8                continue;
-9            }
-10            if (ord < smallest_ord) {
-11                res = c;
-12                smallest_ord = ord;
+6            if (!threshold_reached) {
+7                if (c > target) {
+8                    res = c;
+9                    threshold_reached = !threshold_reached;
+10                }
+11            } else {
+12                if(c > target && c < res) res = c;
 13            }
 14        }
 15
