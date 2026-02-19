d65429925f4fdeccb4f1b5ed633c8874cadf67e6
Yuan <65935215+Acreast@users.noreply.github.com>
2026-02-19 23:30:54 +0800

    File updated at 2/19/2026, 11:31:06 PM

--- 696-CountBinarySubstrings/696-CountBinarySubstrings.java
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.java
@@ -1,28 +1,19 @@
-// Last updated: 2/19/2026, 11:22:12 PM
+// Last updated: 2/19/2026, 11:31:06 PM
 1class Solution {
 2    public int countBinarySubstrings(String s) {
-3        List<Integer> groups = new ArrayList<>();
-4        char prev = s.charAt(0);
-5        int counter = 1;
-6        for (int i = 1; i < s.length(); i++) {
-7            char cur_char = s.charAt(i);
-8            if (cur_char != prev) {
-9                groups.add(counter);
-10                counter = 1;
-11                prev = cur_char;
-12            } else {
-13                counter += 1;
-14            }
-15        }
-16        groups.add(counter);
-17
-18        int prev_group = groups.get(0);
-19        int res = 0;
-20        for (int i = 1; i < groups.size(); i++ ) {
-21            int cur_group = groups.get(i);
-22            res += Math.min(prev_group,cur_group);
-23            prev_group = cur_group;
-24        }
-25        return res;
-26    }
-27}
+3        int res = 0;
+4        int prevGroup = 0;
+5        int curGroup = 1;
+6        for (int i = 1; i < s.length();i ++) {
+7            if (s.charAt(i) == s.charAt(i - 1)) {
+8                curGroup += 1;
+9            } else {
+10                res += Math.min(prevGroup, curGroup);
+11                prevGroup = curGroup;
+12                curGroup = 1;
+13            }
+14        }
+15
+16        return res + Math.min(curGroup,prevGroup);
+17    }
+18}
