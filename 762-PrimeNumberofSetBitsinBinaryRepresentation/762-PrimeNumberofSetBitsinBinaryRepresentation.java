905b4a64edb75cf0a377fb16fa80e9072752fa97
Yuan <65935215+Acreast@users.noreply.github.com>
2026-02-22 21:12:44 +0800

    File updated at 2/22/2026, 9:12:58 PM

--- 762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.java
+++ 762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.java
@@ -1,27 +1,17 @@
-// Last updated: 2/21/2026, 11:45:20 PM
+// Last updated: 2/22/2026, 9:12:58 PM
 1class Solution {
-2    public int countPrimeSetBits(int left, int right) {
-3        int count = 0;
-4        for (int i = left; i <= right; i ++) {
-5            int ones = Integer.bitCount(i);
-6            if (isPrime(ones)) {
-7                count ++;
-8            }
-9        }
-10        return count;
-11
-12    }
-13
-14    public static boolean isPrime(int n) {
-15        if (n <= 1) {
-16            return false;
-17        }
-18
-19        for (int i = 2; i < n; i ++) {
-20            if (n % i == 0) {
-21                return false;
-22            } 
-23        }
-24        return true;
-25    }
-26}
+2    public int binaryGap(int n) {
+3        String string_form_n = Integer.toBinaryString(n);
+4        int prev_one_i = -1;
+5        int res = 0;
+6        for (int i = 0; i < string_form_n.length(); i ++) {
+7            if (string_form_n.charAt(i) == '1') {
+8                if (prev_one_i != -1) {
+9                    res = Math.max(res, i - prev_one_i);
+10                } 
+11                prev_one_i = i;
+12            }
+13        } 
+14        return res;
+15    }
+16}
