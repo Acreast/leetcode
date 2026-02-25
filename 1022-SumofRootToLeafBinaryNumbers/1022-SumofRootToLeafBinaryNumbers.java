877be732b97619fed42fdc6a28212b60ad14d1fb
Yuan <65935215+Acreast@users.noreply.github.com>
2026-02-25 21:26:27 +0800

    File updated at 2/25/2026, 9:26:45 PM

--- 1022-SumofRootToLeafBinaryNumbers/1022-SumofRootToLeafBinaryNumbers.java
+++ 1022-SumofRootToLeafBinaryNumbers/1022-SumofRootToLeafBinaryNumbers.java
@@ -1,34 +1,18 @@
-// Last updated: 2/25/2026, 8:53:57 PM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public int sumRootToLeaf(TreeNode root) {
-18        return dfs(root, 0);
-19    }
-20
-21
-22    private static int dfs(TreeNode node, int curr) {
-23        if (node == null) return 0;
-24
-25        curr = curr * 2 + node.val;
-26
-27        if (node.left == null && node.right == null) {
-28            return curr;
-29        }
-30
-31        return dfs(node.left, curr) + dfs(node.right, curr);
-32    }
-33}
+// Last updated: 2/25/2026, 9:26:45 PM
+1class Solution {
+2    public int[] sortByBits(int[] arr) {
+3        int MAGIC_NUMBER = 10001;
+4
+5        for (int i = 0; i < arr.length; i ++) {
+6            arr[i] += Integer.bitCount(arr[i]) * MAGIC_NUMBER;
+7        }
+8
+9        Arrays.sort(arr);
+10
+11        for (int i = 0; i < arr.length; i ++) {
+12            arr[i] %= MAGIC_NUMBER;
+13        }
+14
+15        return arr;
+16    }
+17}
