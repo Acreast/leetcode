d069310fa80f5ba7182f90df9a077d3fd6ee4db5
Yuan <65935215+Acreast@users.noreply.github.com>
2026-02-28 23:03:58 +0800

    File updated at 2/28/2026, 11:04:18 PM

--- 3666-MinimumOperationstoEqualizeBinaryString/3666-MinimumOperationstoEqualizeBinaryString.java
+++ 3666-MinimumOperationstoEqualizeBinaryString/3666-MinimumOperationstoEqualizeBinaryString.java
@@ -1,42 +1,16 @@
-// Last updated: 2/28/2026, 12:07:53 AM
+// Last updated: 2/28/2026, 11:04:18 PM
 1class Solution {
-2    public int minOperations(String s, int k) {
-3        int zero = 0;
-4        int len = s.length();
-5
-6        for (int i = 0; i < len; i++)
-7            zero += ~s.charAt(i) & 1;
-8
-9        if (zero == 0)
-10            return 0;
+2    public int concatenatedBinary(int n) {
+3        long res = 0;
+4        int MOD = 1_000_000_007;
+5        int bits = 0;
+6
+7        for (int i = 1; i <= n; i ++) {
+8            if ((i & (i - 1)) == 0 ) bits ++;
+9            res = ((res << bits) | i) % MOD;
+10        }
 11
-12        if (len == k)
-13            return (zero == len ? 1 : -1);
-14
-15        int base = len - k;
-16
-17        int odd = Math.max(
-18            (zero + k - 1) / k,
-19            (len - zero + base - 1) / base
-20        );
-21
-22        odd += ~odd & 1;
-23
-24        int even = Math.max(
-25            (zero + k - 1) / k,
-26            (zero + base - 1) / base
-27        );
-28
-29        even += even & 1;
-30
-31        int res = Integer.MAX_VALUE;
-32
-33        if ((k & 1) == (zero & 1))
-34            res = Math.min(res, odd);
-35
-36        if ((~zero & 1) == 1)
-37            res = Math.min(res, even);
-38
-39        return res == Integer.MAX_VALUE ? -1 : res;
-40    }
-41}
+12
+13        return (int) res;
+14    }
+15}
