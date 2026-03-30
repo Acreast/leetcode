570c09d4ac5420f937b8777b67906ab64d8ecc66
Yuan <65935215+Acreast@users.noreply.github.com>
2026-03-30 12:31:54 +0800

    File updated at 3/30/2026, 12:31:58 PM

--- 2839-CheckifStringsCanbeMadeEqualWithOperationsI/2839-CheckifStringsCanbeMadeEqualWithOperationsI.java
+++ 2839-CheckifStringsCanbeMadeEqualWithOperationsI/2839-CheckifStringsCanbeMadeEqualWithOperationsI.java
@@ -1,27 +1,26 @@
-// Last updated: 3/29/2026, 7:38:39 PM
+// Last updated: 3/30/2026, 12:31:58 PM
 1class Solution {
-2    public boolean canBeEqual(String s1, String s2) {
-3        
-4        int[] even = new int[26];
-5        int[] odd = new int[26];
-6
-7        for (int i = 0; i < s1.length(); i++) {
-8            if (i % 2 == 0) {
-9                even[s1.charAt(i) - 'a']++;
-10                even[s2.charAt(i) - 'a']--;
-11            } else {
-12                odd[s1.charAt(i) - 'a']++;
-13                odd[s2.charAt(i) - 'a']--;
-14            }
-15        }
-16
-17        for (int i = 0; i < 26; i++) {
-18            if (even[i] != 0 || odd[i] != 0) {
-19                return false;
-20            }
-21        }
-22
-23        return true;
-24        
-25    }
-26}
+2    public boolean checkStrings(String s1, String s2) {
+3        int[] even = new int[26];
+4        int[] odd = new int[26];
+5
+6        for (int i = 0; i < s1.length(); i++) {
+7            if (i % 2 == 0) {
+8                even[s1.charAt(i) - 'a']++;
+9                even[s2.charAt(i) - 'a']--;
+10            } else {
+11                odd[s1.charAt(i) - 'a']++;
+12                odd[s2.charAt(i) - 'a']--;
+13            }
+14        }
+15
+16        for (int i = 0; i < 26; i++) {
+17            if (even[i] != 0 || odd[i] != 0) {
+18                return false;
+19            }
+20        }
+21
+22        return true;
+23        
+24    }
+25}
