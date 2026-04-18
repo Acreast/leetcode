bdb0d171746d85c4f7fd6f4740d601b2b45ad65e
Yuan <65935215+Acreast@users.noreply.github.com>
2026-04-18 18:52:15 +0800

    File updated at 4/18/2026, 6:52:24 PM

--- 3761-MinimumAbsoluteDistanceBetweenMirrorPairs/3761-MinimumAbsoluteDistanceBetweenMirrorPairs.java
+++ 3761-MinimumAbsoluteDistanceBetweenMirrorPairs/3761-MinimumAbsoluteDistanceBetweenMirrorPairs.java
@@ -1,27 +1,20 @@
-// Last updated: 4/18/2026, 12:04:48 AM
+// Last updated: 4/18/2026, 6:52:24 PM
 1class Solution {
-2    public int minMirrorPairDistance(int[] nums) {
-3        int minDist = Integer.MAX_VALUE;
-4        Map<Integer, Integer> map = new HashMap<>();
-5        int i = 0;
-6        for (int n : nums) {
-7            if (map.containsKey(n)) {
-8                minDist = Math.min(minDist, i - map.get(n));
-9            }
-10            int rev = reverse(n);
-11            map.put(rev, i);
-12            i++;
-13        }
-14        return minDist == Integer.MAX_VALUE ? -1 : minDist;
-15    }
-16    
-17    private static int reverse(int num) {
-18        int rev = 0;
-19        while (num > 0) {
-20            rev = rev * 10 + num % 10;
-21            num /= 10;
-22        }
-23        return rev;
-24    }
-25
-26}
+2    public int mirrorDistance(int n) {
+3        int r = reverse(n);
+4        return Math.abs(n - r);
+5    }
+6
+7    private static int reverse(int num) {
+8        int rev = 0;
+9
+10        while (num > 0) {
+11            rev = rev * 10  + num % 10;
+12            num /= 10;
+13        }   
+14
+15        return rev;
+16    }
+17
+18    
+19}
