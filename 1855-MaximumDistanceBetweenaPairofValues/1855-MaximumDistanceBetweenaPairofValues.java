950ccb9886367586d064e3af196b9717c767993c
Yuan <65935215+Acreast@users.noreply.github.com>
2026-04-20 23:23:47 +0800

    File updated at 4/20/2026, 11:24:00 PM

--- 1855-MaximumDistanceBetweenaPairofValues/1855-MaximumDistanceBetweenaPairofValues.java
+++ 1855-MaximumDistanceBetweenaPairofValues/1855-MaximumDistanceBetweenaPairofValues.java
@@ -1,23 +1,25 @@
-// Last updated: 4/20/2026, 12:41:25 AM
+// Last updated: 4/20/2026, 11:24:00 PM
 1class Solution {
-2    public int maxDistance(int[] nums1, int[] nums2) {
-3        int maxDist = Integer.MIN_VALUE, n = nums1.length, m = nums2.length;
-4
-5        int i = 0, j = 0;
-6        while (i < n && j < m) {
-7            if (i <= j && nums1[i] <= nums2[j]) {
-8                int dist = j - i;
-9                maxDist = Math.max(maxDist, dist);
-10                j++;
-11            } else {
-12                if (i <= j && nums1[i] > nums2[j]) {
-13                    i++;
-14                } else if (i > j) {
-15                    j++;
-16                }
-17            }
+2    public int maxDistance(int[] colors) {
+3        ArrayList<Integer> seen = new ArrayList<Integer>();
+4        int res = -Integer.MAX_VALUE;
+5        for(int i = 0; i < colors.length - 1; i ++) {
+6            int cur_color = colors[i];
+7            if (seen.contains(cur_color)) {
+8                continue;
+9            }
+10            seen.add(cur_color);
+11            for (int j = i + 1; j < colors.length; j ++) {
+12                int next_color = colors[j];
+13                if (cur_color != next_color) {
+14                    res = Math.max(res, j - i);
+15                }
+16            }
+17            
 18        }
 19
-20        return maxDist == Integer.MIN_VALUE ? 0 : maxDist;
-21    }
-22}
+20        return res;
+21
+22
+23    }
+24}
