7e1da4b6700ee39136ad029930b1333a5e036031
Yuan <65935215+Acreast@users.noreply.github.com>
2026-04-25 23:42:20 +0800

    Create 2833-FurthestPointFromOrigin

---
+++ 2833-FurthestPointFromOrigin/2833-FurthestPointFromOrigin.java
@@ -0,0 +1,23 @@
+// Last updated: 4/25/2026, 11:42:19 PM
+1class Solution {
+2    public int furthestDistanceFromOrigin(String moves) {
+3        int n = moves.length();
+4        int countL = 0;
+5        int countR = 0;
+6        int countUnderscore = 0;
+7
+8        for (int i = 0; i < n; i ++) {
+9            char c = moves.charAt(i);
+10            if (c == 'L') {
+11                countL += 1;
+12            } else if(c == 'R') {
+13                countR += 1;
+14            } else if (c == '_') {
+15                countUnderscore += 1;
+16            }
+17        }
+18        int positionAllL = (countL + countUnderscore) - countR;
+19        int positionAllR = countL - (countR + countUnderscore);
+20        return Math.max(Math.abs(positionAllL), Math.abs(positionAllR));
+21    }
+22}
