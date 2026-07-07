03e45fafb283c2ca3c7d5ecb4b1e7840e79247e5
Yuan <65935215+Acreast@users.noreply.github.com>
2026-07-08 00:38:27 +0800

    File updated at 7/8/2026, 12:39:05 AM

--- 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI/3754-ConcatenateNon-ZeroDigitsandMultiplybySumI.java
+++ 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI/3754-ConcatenateNon-ZeroDigitsandMultiplybySumI.java
@@ -1,25 +1,19 @@
-// Last updated: 7/8/2026, 12:35:58 AM
+// Last updated: 7/8/2026, 12:39:05 AM
 1class Solution {
 2    public long sumAndMultiply(int n) {
 3        int cur = n;
-4        int sum = 0;
-5        int res = 0;
-6        while ( cur != 0) {
-7            int digit = cur % 10;
-8            if (digit == 0) {
-9                cur /= 10;
-10                continue;
-11            }
-12                
-13            res = res * 10 + digit;
-14            sum = sum + digit;
-15            cur /= 10;
-16        }
-17        long reversedRes = 0;
-18        while (res > 0) {
-19            reversedRes = reversedRes * 10 + (res % 10);
-20            res /= 10;
-21        }
-22        return reversedRes * sum;
-23    }
-24}
+4        long sum = 0;
+5        long res = 0;
+6        long multiplier = 1;
+7        while ( cur != 0) {
+8            int digit = cur % 10;
+9            if (digit != 0) {
+10                res = digit * multiplier + res; 
+11                sum += digit;
+12                multiplier *= 10; 
+13            }
+14            cur /= 10;
+15        }
+16        return res * sum;
+17    }
+18}
