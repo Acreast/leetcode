6c42ab8e83b922e6163edf2bbad884de034a3976
Yuan <65935215+Acreast@users.noreply.github.com>
2026-07-27 21:31:34 +0800

    File updated at 7/27/2026, 9:31:35 PM

--- 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.py
+++ 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.py
@@ -1,16 +1,12 @@
-# Last updated: 7/26/2026, 1:45:56 AM
+# Last updated: 7/27/2026, 9:31:35 PM
 1class Solution:
-2    def maxProduct(self, n: int) -> int:
-3        max1 = max2 = 0
-4        
-5        while n > 0:
-6            digit = n % 10
-7            n //= 10
-8            
-9            if digit > max1:
-10                max2 = max1
-11                max1 = digit
-12            elif digit > max2:
-13                max2 = digit
-14                
-15        return max1 * max2
+2    def maxProduct(self, nums: List[int]) -> int:
+3        max1, max2 = 0, 0
+4        for num in nums:
+5            if num > max1:
+6                max2 = max1
+7                max1 = num
+8            elif num > max2:
+9                max2 = num
+10        
+11        return (max1 - 1) * (max2 - 1)
