01d7af602150ce556ede4896b8a33beec8f01616
Yuan <65935215+Acreast@users.noreply.github.com>
2026-07-29 00:13:45 +0800

    Create 3517-SmallestPalindromicRearrangementI

---
+++ 3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.py
@@ -0,0 +1,10 @@
+# Last updated: 7/29/2026, 12:13:46 AM
+1class Solution:
+2    def smallestPalindrome(self, s: str) -> str:
+3        n = len(s)
+4        freq = Counter(s[:n >> 1])
+5
+6        half = "".join(c * freq[c] for c in ascii_lowercase)
+7        mid = s[n >> 1] if n & 1 else ""
+8
+9        return half + mid + half[::-1]
