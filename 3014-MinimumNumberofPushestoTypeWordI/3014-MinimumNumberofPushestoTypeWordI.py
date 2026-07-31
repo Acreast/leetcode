4748cb37fd68a91e05d500be569684907d4c985d
Yuan <65935215+Acreast@users.noreply.github.com>
2026-08-01 01:01:18 +0800

    File updated at 8/1/2026, 1:01:24 AM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.py
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.py
@@ -1,6 +1,4 @@
-# Last updated: 7/31/2026, 12:43:27 AM
+# Last updated: 8/1/2026, 1:01:24 AM
 1class Solution:
 2    def minimumPushes(self, word: str) -> int:
-3        n = len(word)
-4        blocks = n // 8
-5        return (blocks * (blocks + 1) * 4) + (n % 8) * (blocks + 1)
+3        return sum(f*(i//8+1) for i, f in enumerate(sorted(Counter(word).values(), reverse=True)))
