905beba4074411d9ba2132f0535bf3a55ba4b441
Yuan <65935215+Acreast@users.noreply.github.com>
2026-08-07 00:54:23 +0800

    File updated at 8/7/2026, 12:54:37 AM

--- 3345-SmallestDivisibleDigitProductI/3345-SmallestDivisibleDigitProductI.py
+++ 3345-SmallestDivisibleDigitProductI/3345-SmallestDivisibleDigitProductI.py
@@ -1,4 +1,4 @@
-# Last updated: 8/7/2026, 12:54:11 AM
+# Last updated: 8/7/2026, 12:54:37 AM
 1class Solution:
 2    def smallestNumber(self, n: int, t: int) -> int:
 3        while True:
@@ -6,12 +6,11 @@
 5            prod = n % 10
 6            cur = n // 10
 7            while cur > 0:
-8                digit = cur % 10
-9                prod *= digit
-10                cur //= 10
-11            if prod % t == 0:
-12                return n
-13            n += 1
-14        return 0
+8                prod *= cur % 10
+9                cur //= 10
+10            if prod % t == 0:
+11                return n
+12            n += 1
+13        return 0
+14
 15
-16
