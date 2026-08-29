c60a0a3ee519b49e8ef5078b3d9c3bdcaeede2f5
Yuan <65935215+Acreast@users.noreply.github.com>
2026-08-30 00:28:49 +0800

    File updated at 8/30/2026, 12:28:59 AM

--- 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.py
+++ 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.py
@@ -1,20 +1,20 @@
-class Solution:
-    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        groups = []
-        num_to_group = {}
-        
-        for n in sorted(nums):
-            if not groups or abs(n - groups[-1][-1]) > limit:
-                groups.append(deque())
-
-            groups[-1].append(n)
-            num_to_group[n] = len(groups) - 1
-
-
-        res = []
-
-        for n in nums:
-            j = num_to_group[n]
-            res.append(groups[j].popleft())
-        return res
-        
+# Last updated: 8/30/2026, 12:28:59 AM
+1class Solution:
+2    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+3        groups = []
+4        num_to_groups = {}
+5
+6        for n in sorted(nums):
+7            if not groups or abs(n - groups[-1][-1]) > limit:
+8                groups.append(deque())
+9
+10            groups[-1].append(n)
+11            num_to_groups[n] = len(groups) - 1
+12
+13        res = []
+14
+15        for n in nums:
+16            j = num_to_groups[n]
+17            res.append(groups[j].popleft())
+18
+19        return res
