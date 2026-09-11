93b72a1684f2539aa1f67298e1330729ac48af0f
Yuan <65935215+Acreast@users.noreply.github.com>
2026-09-12 00:22:49 +0800

    File updated at 9/12/2026, 12:23:21 AM

--- 2265-CountNodesEqualtoAverageofSubtree/2265-CountNodesEqualtoAverageofSubtree.py
+++ 2265-CountNodesEqualtoAverageofSubtree/2265-CountNodesEqualtoAverageofSubtree.py
@@ -1,38 +1,25 @@
-# Last updated: 9/11/2026, 12:36:03 AM
-1# Definition for a binary tree node.
-2# class TreeNode:
-3#     def __init__(self, val=0, left=None, right=None):
-4#         self.val = val
-5#         self.left = left
-6#         self.right = right
-7class Solution:
-8    def averageOfSubtree(self, root: TreeNode) -> int:
-9        res = 0
-10        def dfs(node):
-11            nonlocal res
-12            if not node:
-13                return [0,0]
-14            leftTree = dfs(node.left)
-15            rightTree = dfs(node.right)
-16
-17            sum = leftTree[0] + rightTree[0]  + node.val
-18            nodeNum = leftTree[1] + rightTree[1] + 1
-19
-20            if sum // nodeNum == node.val:
-21                res += 1
-22            
-23            return [sum, nodeNum]
-24        
-25        dfs(root)
-26        return res
-27
-28        
-29
-30
-31
-32
-33
-34
-35
-36
-37
+# Last updated: 9/12/2026, 12:23:21 AM
+1class Solution:
+2    def totalNumbers(self, digits: List[int]) -> int:
+3        n = len(digits)
+4        seen = set()
+5
+6        for h in range(n):
+7            if digits[h] == 0:
+8                continue
+9            
+10            for t in range(n):
+11                if t == h:
+12                    continue
+13                
+14                for u in range(n):
+15                    if u == h or u == t:
+16                        continue
+17                    
+18                    if digits[u] % 2 != 0:
+19                        continue
+20                    
+21                    num = digits[h] * 100 + digits[t] * 10 + digits[u]
+22                    seen.add(num)
+23        
+24        return len(seen)
